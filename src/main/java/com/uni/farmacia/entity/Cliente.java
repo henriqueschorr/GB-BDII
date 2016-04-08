91f901e61e025aa13069bd9939f0af216f1d0252
henriqueschorr <henrique.schorr@gmail.com>
2016-04-07 21:37:40 -0300

    Initial state

---
+++ src/main/java/com/uni/farmacia/entity/Cliente.java
@@ -0,0 +1,20 @@
+package com.uni.farmacia.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+public class Cliente implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_Classificacao")
+	@SequenceGenerator(name = "seq_Classificacao", sequenceName = "s_Classificacao", allocationSize = 1)
+	private Long id;
+	@Column(length = 50, nullable = false)
+	private String nome;
+
+}
